3e31f07aafc2633aa91acb5e7859a0a325639200
0xAdachi <0xadachisakura@gmail.com>
2024-07-12 17:06:01 +0600

    Updated pause logic

--- content.js
+++ content.js
@@ -1,12 +1,21 @@
-document.addEventListener('visibilitychange', () => {
-  const video = document.querySelector('video');
-  if (document.visibilityState === 'hidden') {
-      if (video) {
-          video.pause();
-      }
-  } else if (document.visibilityState === 'visible') {
-      if (video) {
-          video.play();
-      }
-  }
-});
+const video = document.querySelector('video');
+
+function handleVisibilityChange() {
+    if (document.visibilityState === 'hidden') {
+        if (video) video.pause();
+    } else if (document.visibilityState === 'visible') {
+        if (document.hasFocus() && video) video.play();
+    }
+}
+
+function handleWindowBlur() {
+    if (video) video.pause();
+}
+
+function handleWindowFocus() {
+    if (document.visibilityState === 'visible' && video) video.play();
+}
+
+document.addEventListener('visibilitychange', handleVisibilityChange);
+window.addEventListener('blur', handleWindowBlur);
+window.addEventListener('focus', handleWindowFocus);
